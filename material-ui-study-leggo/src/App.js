d33b0f4b6bd7bd190c405a36ec82417ced811b65
yunchan312 <phenomenal312@naver.com>
2021-01-01 17:51:28 +0900

    material-ui-study

---
+++ material-ui-study-leggo/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Drawer from './drawer';
+
+function App() {
+  return (
+    <div className="App">
+      <Drawer />
+    </div>
+  );
+}
+
+export default App;
