177da56d33bfbb0f51127e66f8888ef028f01437
yunchan312 <phenomenal312@naver.com>
2021-01-05 12:46:58 +0900

    footer

--- makssg2/src/App.js
+++ makssg2/src/App.js
@@ -7,6 +7,9 @@ import Reports from './pages/Reports';
 import Products from './pages/Products';
 import Dropdown1 from './components/dropdown1';
 import Dropdown2 from './components/dropdown2';
+import { SiFacebook } from 'react-icons/si';
+import { RiInstagramFill } from 'react-icons/ri';
+import { AiFillTwitterCircle } from 'react-icons/ai';
 
 function App() {
   return (
@@ -29,8 +32,27 @@ function App() {
           <Route path='/products' component={Products} />
         </Switch>
 
+        <div className="footer">
+            <div className="socials">
+              <a href="https://www.facebook.com" style={{color: '#deb887'}} className="icons">
+                <SiFacebook size={28}/>
+              </a>
+              <a href="https://www.instagram.com" style={{color: '#deb887'}} className="icons">
+                <RiInstagramFill size={30}/>
+              </a>
+              <a href="https://www.twitter.com" style={{color: '#deb887'}} className="icons">
+                <AiFillTwitterCircle size={30}/>
+              </a>
+            </div>
 
+          <div className="footer-terms">
+            홈페이지 이용약관 | 영상정보처리기기 운영방침 | 멤버스카드 이용약관 | 위치기반 서비스 이용약관
+          </div>
 
+          <div className="footer-info">
+            판매자 정보s
+          </div>
+        </div>
       </Router>
 
     </>
